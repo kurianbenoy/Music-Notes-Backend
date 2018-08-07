de8bafeaee90d5c0f93f4c7e782f1aa6eb41aa77
kurianbenoy <kurian.pro@gmail.com>
2018-08-08 00:42:33 +0530

    Made first random tester without timeout

--- backendnotes/core/views.py
+++ backendnotes/core/views.py
@@ -1,3 +1,10 @@
+import random
 from django.shortcuts import render
-
+from django.http import HttpResponse
+#from django
 # Create your views here.
+
+def tester(request):
+    a = {1:'C',2:'D',3:'E',4:'F',5:'G',6:'H'}
+    selector = random.randint(1,6)
+    return (HttpResponse(a[selector]))
